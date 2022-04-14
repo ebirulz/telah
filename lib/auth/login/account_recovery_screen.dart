4d844b77b1cfe81367cf80b0e73b054b972f8b65
Ebirulz <ebi.okpongu@gmail.com>
2022-04-14 16:10:14 +0100

    Login UI
    Login UI Completed

---
+++ lib/auth/login/account_recovery_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AccountRecoveryScreen extends StatefulWidget {
+  const AccountRecoveryScreen({Key? key}) : super(key: key);
+
+  @override
+  _AccountRecoveryScreenState createState() => _AccountRecoveryScreenState();
+}
+
+class _AccountRecoveryScreenState extends State<AccountRecoveryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
